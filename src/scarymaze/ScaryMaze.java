b1403b24a5eaeb42af521c0da7a465bdfdebedf0
namrita <namrita@10.0.1.3>
2011-09-11 16:05:30 -0700

    added mouse motion listener stuff

--- src/scarymaze/ScaryMaze.java
+++ src/scarymaze/ScaryMaze.java
@@ -4,9 +4,13 @@
  */
 package scarymaze;
 
+import java.awt.AWTException;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Robot;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
@@ -14,8 +18,8 @@ import javax.swing.JFrame;
  *
  * @author avh4
  */
-public class ScaryMaze extends JComponent {
-    
+public class ScaryMaze extends JComponent implements MouseMotionListener {
+    private Robot r;
     private Color BRIGHT_PINK = new Color(255, 60, 140);
     private Color DARK_BLUE = new Color (0, 51, 102);
     private Color GREEN = new Color (0, 255, 0);
@@ -23,13 +27,18 @@ public class ScaryMaze extends JComponent {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AWTException {
         JFrame window = new JFrame("Maze Game");
-        window.add(new ScaryMaze());
+        ScaryMaze game = new ScaryMaze();
+        window.add(game);
+        game.addMouseMotionListener(game);
         window.pack();
         window.setLocationRelativeTo(null); // Center on the screen
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
+    }
+    public ScaryMaze () throws AWTException {
+        r = new Robot();
     }
 
     @Override
@@ -52,6 +61,40 @@ public class ScaryMaze extends JComponent {
         
         
         
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e)
+    {
+        Color c = r.getPixelColor(e.getXOnScreen(), e.getYOnScreen());
+        System.out.println(c);
+        if (colorsMatch(BRIGHT_PINK, c))
+        {
+            System.out.println("wall");
+        }
+        else if (c.equals(GREEN))
+        {
+            System.out.println("salvation");
+        }
+    }
+    
+    private boolean colorsMatch(Color a, Color b)
+    {
+        if (a.getRed() == b.getRed() && a.getGreen() == b.getGreen() && a.getBlue() == b.getBlue())
+        {
+            return true;
+           
+        }
+        else {
+            return false;
+        }
+  
     }
     
     
